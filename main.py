7a81b04759a1da2038a612c69fb0b452dbc07dc7
Critter & Guitari <critter@critterandguitari.com>
2015-10-17 04:43:04 +0000

    changed mvp to etc in .py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pygame
 import time
 import random
 import glob
-import mvp_system
+import etc_system
 import imp
 import socket
 import traceback
@@ -12,9 +12,9 @@ import liblo
 from pygame.locals import *
 import alsaaudio, audioop
  
-#create mvp object
-mvp = mvp_system.System()
-mvp.clear_flags()
+#create etc object
+etc = etc_system.System()
+etc.clear_flags()
 
 # OSC init
 try:
@@ -32,61 +32,61 @@ def fallback(path, args):
 def knobs_callback(path, args):
     global osc_msgs_recv
     osc_msgs_recv += 1
-    global mvp
+    global etc
     k1, k2, k3, k4, k5, k6 = args
-    mvp.knob1l = float(k4) / 1023
-    mvp.knob2l = float(k1) / 1023
-    mvp.knob3l = float(k2) / 1023
-    mvp.knob4l = float(k5) / 1023
-    mvp.knob5l = float(k3) / 1023
+    etc.knob1l = float(k4) / 1023
+    etc.knob2l = float(k1) / 1023
+    etc.knob3l = float(k2) / 1023
+    etc.knob4l = float(k5) / 1023
+    etc.knob5l = float(k3) / 1023
 
 def keys_callback(path, args) :
     global osc_msgs_recv
     osc_msgs_recv += 1
-    global mvp
+    global etc
     k, v = args
-    if (k == 2 and v > 0) : mvp.next_patch = True
-    if (k == 1 and v > 0) : mvp.prev_patch = True
-    if (k == 9 and v > 0) : mvp.clear_screen = True
-    if (k == 7 and v > 0) : mvp.screengrab = True
-    if (k == 4 and v > 0) : mvp.prev_preset()
-    if (k == 6 and v > 0) : mvp.save_preset()
-    if (k == 5 and v > 0) : mvp.next_preset()
+    if (k == 2 and v > 0) : etc.next_patch = True
+    if (k == 1 and v > 0) : etc.prev_patch = True
+    if (k == 9 and v > 0) : etc.clear_screen = True
+    if (k == 7 and v > 0) : etc.screengrab = True
+    if (k == 4 and v > 0) : etc.prev_preset()
+    if (k == 6 and v > 0) : etc.save_preset()
+    if (k == 5 and v > 0) : etc.next_preset()
     if (k == 3 and v > 0) : 
-        if (mvp.osd) : mvp.osd = False
-        else : mvp.osd = True
+        if (etc.osd) : etc.osd = False
+        else : etc.osd = True
     if (k == 8 and v > 0) : 
-        if (mvp.auto_clear) : mvp.auto_clear = False
-        else : mvp.auto_clear = True
+        if (etc.auto_clear) : etc.auto_clear = False
+        else : etc.auto_clear = True
 
     print str(k) + " " + str(v)
 
 def midi_note_on_callback(path, args):
     global osc_msgs_recv
     osc_msgs_recv += 1
-    global mvp
+    global etc
     c, n, v = args
-    mvp.note_on = True
-    mvp.note_num = n
-    mvp.note_velocity = v
+    etc.note_on = True
+    etc.note_num = n
+    etc.note_velocity = v
     #print n
 
 def midi_cc_callback(path, args):
     global osc_msgs_recv
     osc_msgs_recv += 1
-    global mvp
+    global etc
     c, n, v = args
 
     if n == 21 :
-        mvp.knob1l = float(v) / 127
+        etc.knob1l = float(v) / 127
     if n == 22 :
-        mvp.knob2l = float(v) / 127
+        etc.knob2l = float(v) / 127
     if n == 23 :
-        mvp.knob3l = float(v) / 127
+        etc.knob3l = float(v) / 127
     if n == 24 :
-        mvp.knob4l = float(v) / 127
+        etc.knob4l = float(v) / 127
     if n == 25 :
-        mvp.knob5l = float(v) / 127
+        etc.knob5l = float(v) / 127
 #    print args
 
 
@@ -159,7 +159,7 @@ for patch_folder in patch_folders :
 
 # set initial patch
 num = 0
-mvp.patch = patch_names[num]
+etc.patch = patch_names[num]
 patch = sys.modules[patch_names[num]]
 
 # run setup functions if patches have them
@@ -167,7 +167,7 @@ patch = sys.modules[patch_names[num]]
 for patch_name in patch_names :
     try :
         patch = sys.modules[patch_name]
-        patch.setup(screen, mvp)
+        patch.setup(screen, etc)
     except AttributeError :
         print "no setup found"
         continue 
@@ -175,13 +175,13 @@ for patch_name in patch_names :
 
 # recent grabs
 print 'loading recent grabs...'
-mvp.tengrabs = []
-mvp.tengrabs_thumbs = []
-mvp.grabcount = 0
-mvp.grabindex = 0
+etc.tengrabs = []
+etc.tengrabs_thumbs = []
+etc.grabcount = 0
+etc.grabindex = 0
 for i in range(0,11):
-    mvp.tengrabs_thumbs.append(pygame.Surface((128, 72)))
-    mvp.tengrabs.append(pygame.Surface(hwscreen.get_size()))
+    etc.tengrabs_thumbs.append(pygame.Surface((128, 72)))
+    etc.tengrabs.append(pygame.Surface(hwscreen.get_size()))
 
 for filepath in sorted(glob.glob('/usbdrive/Grabs/*.jpg')):
     filename = os.path.basename(filepath)
@@ -190,10 +190,10 @@ for filepath in sorted(glob.glob('/usbdrive/Grabs/*.jpg')):
     img = img.convert()
     thumb = pygame.transform.scale(img, (128, 72) )
     #TODO : ensure img is 1280 x 720
-    mvp.tengrabs[mvp.grabcount]= img
-    mvp.tengrabs_thumbs[mvp.grabcount] = thumb
-    mvp.grabcount += 1
-    if mvp.grabcount > 10: break
+    etc.tengrabs[etc.grabcount]= img
+    etc.tengrabs_thumbs[etc.grabcount] = thumb
+    etc.grabcount += 1
+    if etc.grabcount > 10: break
 
 
 
@@ -217,7 +217,7 @@ while 1:
     #osc_server.recv(0)
 
     # get knobs from hardware or preset
-    mvp.update_knobs()
+    etc.update_knobs()
 
     # quit on esc
     for event in pygame.event.get():
@@ -246,7 +246,7 @@ while 1:
                 avg = avg / 3
                 if (avg > 1000) :
                     triggered = True
-                mvp.audio_in[i] = avg
+                etc.audio_in[i] = avg
             except :
                 pass
         l,data = inp.read()
@@ -258,52 +258,52 @@ while 1:
         if '\n' in buf :
             lines = buf.split('\n')
             for l in lines :
-                mvp.parse_serial(l)
+                etc.parse_serial(l)
             buf = lines[-1]
     except :
         pass
 
 
-    if mvp.next_patch: 
+    if etc.next_patch: 
         error = ''
         num += 1
         if num == len(patch_names) : 
             num = 0
-        mvp.patch = patch_names[num]
+        etc.patch = patch_names[num]
         patch = sys.modules[patch_names[num]]
-    if mvp.prev_patch: 
+    if etc.prev_patch: 
         error = ''
         num -= 1
         if num < 0 : 
             num = len(patch_names) - 1
-        mvp.patch = patch_names[num]
+        etc.patch = patch_names[num]
         patch = sys.modules[patch_names[num]]
 
-    if mvp.clear_screen:
-        screen.fill(mvp.bg_color) 
+    if etc.clear_screen:
+        screen.fill(etc.bg_color) 
 
-    if mvp.auto_clear :
-        screen.fill(mvp.bg_color) 
+    if etc.auto_clear :
+        screen.fill(etc.bg_color) 
 
-    mvp.bg_color =  mvp.color_picker_bg()
+    etc.bg_color =  etc.color_picker_bg()
     
     # set patch
-    if mvp.set_patch :
+    if etc.set_patch :
         error = ''
-        print "setting: " + mvp.patch
+        print "setting: " + etc.patch
         try :
-            patch = sys.modules[mvp.patch]
+            patch = sys.modules[etc.patch]
         except KeyError:
-            error = "Module " +mvp.patch+ " is not loaded, probably it has errors"
+            error = "Module " +etc.patch+ " is not loaded, probably it has errors"
 
     # reload
-    if mvp.reload_patch :
+    if etc.reload_patch :
         error = ''
         # delete the old
-        if mvp.patch in sys.modules:  
-            del(sys.modules[mvp.patch]) 
+        if etc.patch in sys.modules:  
+            del(sys.modules[etc.patch]) 
         print "deleted module, reloading"
-        patch_name = mvp.patch
+        patch_name = etc.patch
         patch_path = '/usbdrive/Patches/'+patch_name+'/main.py'
         try :
             patch = imp.load_source(patch_name, patch_path)
@@ -311,19 +311,19 @@ while 1:
             
             # then call setup
             try :
-                patch.setup(screen, mvp)
+                patch.setup(screen, etc)
             except Exception, e:
                 error = traceback.format_exc()
         except Exception, e:
             error = traceback.format_exc()
     
     try :
-        patch.draw(screen, mvp)
+        patch.draw(screen, etc)
     except Exception, e:
         error = traceback.format_exc()
  
     #save frame
-    if mvp.screengrab:
+    if etc.screengrab:
         filenum = 0
         imagepath = "/usbdrive/Grabs/" + str(filenum) + ".jpg"
         imagepath_thumb = "/usbdrive/Grabs/" + str(filenum) + "_thumb.jpg"
@@ -334,18 +334,18 @@ while 1:
         pygame.image.save(screen,imagepath)
         # add to the grabs array
         #grab = screen
-        mvp.grabindex += 1
-        mvp.grabindex %= 10
-        pygame.transform.scale(screen, (128, 72), mvp.tengrabs_thumbs[mvp.grabindex] )
-        pygame.image.save(mvp.tengrabs_thumbs[mvp.grabindex],imagepath_thumb)
-        mvp.tengrabs[mvp.grabindex] = screen.copy()
+        etc.grabindex += 1
+        etc.grabindex %= 10
+        pygame.transform.scale(screen, (128, 72), etc.tengrabs_thumbs[etc.grabindex] )
+        pygame.image.save(etc.tengrabs_thumbs[etc.grabindex],imagepath_thumb)
+        etc.tengrabs[etc.grabindex] = screen.copy()
         print imagepath
    
     #draw the main screen
     hwscreen.blit(screen, (0,0))
     
     # osd
-    if mvp.osd :
+    if etc.osd :
         this_time = time.time()
         elapsed_time = this_time - last_time
         last_time = this_time
@@ -353,14 +353,14 @@ while 1:
         #osd.fill(GREEN) 
         pygame.draw.rect(hwscreen, OSDBG, (0, hwscreen.get_height() - 40, hwscreen.get_width(), 40))
         font = pygame.font.SysFont(None, 32)
-        text = font.render(str(patch.__name__) + ', frame: ' + str(count) + ', fps: ' + str(int(fps)) + ', Auto Clear: ' + str(mvp.auto_clear) + ', osc: ' + str(osc_msgs_recv) + ', osc / sec: ' + str(osc_msgs_per_sec), True, WHITE, OSDBG)
+        text = font.render(str(patch.__name__) + ', frame: ' + str(count) + ', fps: ' + str(int(fps)) + ', Auto Clear: ' + str(etc.auto_clear) + ', osc: ' + str(osc_msgs_recv) + ', osc / sec: ' + str(osc_msgs_per_sec), True, WHITE, OSDBG)
         text_rect = text.get_rect()
         text_rect.x = 50
         text_rect.centery = hwscreen.get_height() - 20
         hwscreen.blit(text, text_rect)
 
         for i in range(0,10) :
-            hwscreen.blit(mvp.tengrabs_thumbs[i], (128 * i,0))
+            hwscreen.blit(etc.tengrabs_thumbs[i], (128 * i,0))
 
         # osd, errors
         i = 0
@@ -375,11 +375,11 @@ while 1:
     #clocker.tick(35)
     pygame.display.flip()
 
-    if mvp.quit :
+    if etc.quit :
         sys.exit()
     
     # clear all the events
-    mvp.clear_flags()
+    etc.clear_flags()
     osc_msgs_recv = 0
 
 time.sleep(1)
